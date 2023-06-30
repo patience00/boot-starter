0297da63e474ab56e2ad5f66164d2650dcb578c8
linch <523163881@qq.com>
2023-06-30 21:34:34 +0800

    参数校验，提示

---
+++ src/main/java/com/linchtech/boot/starter/common/annotations/EnumValuesValidator.java
@@ -0,0 +1,50 @@
+package com.linchtech.boot.starter.common.annotations;
+
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * @author 107
+ * @date 2019/3/12 10:08
+ */
+public class EnumValuesValidator implements ConstraintValidator<EnumValueCheck,Object> {
+
+    private EnumValueCheck enumValueCheck;
+
+    @Override
+    public void initialize(EnumValueCheck enumValueCheck) {
+        this.enumValueCheck = enumValueCheck;
+    }
+
+    /**
+     * Implements the validation logic.
+     * The state of {@code value} must not be altered.
+     * <p>
+     * This method can be accessed concurrently, thread-safety must be ensured
+     * by the implementation.
+     *
+     * @param value   object to validate
+     * @param context context in which the constraint is evaluated
+     * @return {@code false} if {@code value} does not pass the constraint
+     */
+    @Override
+    public boolean isValid(Object value,
+                           ConstraintValidatorContext context) {
+        if (value instanceof String) {
+            for (String allowedValue : this.enumValueCheck.allowedValues()) {
+                if (value.toString().equals(allowedValue)) {
+                    return true;
+                }
+            }
+        }
+        if (value instanceof Integer) {
+            for (String allowedValue : this.enumValueCheck.allowedValues()) {
+                if (value.toString().equals(allowedValue)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
