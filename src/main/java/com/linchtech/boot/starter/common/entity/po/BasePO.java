505e1688943ea9bdc4cc64b37c66630b3274099e
523163881@qq.com <523163881@qq.com>
2024-07-17 21:35:47 +0800

    序列

--- src/main/java/com/linchtech/boot/starter/common/entity/po/BasePO.java
+++ src/main/java/com/linchtech/boot/starter/common/entity/po/BasePO.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,12 +24,17 @@ public class BasePO implements Serializable {
 
     @TableId(type = IdType.AUTO)
     private Long id;
+
     @TableField("gmt_create")
+    @JsonIgnore
     private Date gmtCreate;
+
     @TableField("gmt_modified")
+    @JsonIgnore
     private Date gmtModified;
 
     @TableField(value = "delete_flag", select = false)
     @TableLogic
+    @JsonIgnore
     private Boolean deleteFlag;
 }
